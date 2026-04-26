517bc8de410d4545adc8d57ed0c0b47ed93058d9
Sergio Castro <84090077+Nanookgamer@users.noreply.github.com>
2026-04-26 19:14:48 +0200

    Actualizar la bbdd para poder usar la url interna de render

--- server/db.ts
+++ server/db.ts
@@ -3,19 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool(
-  process.env.NODE_ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      }
-    : {
-        host:     process.env.DB_HOST,
-        port:     Number(process.env.DB_PORT) || 5432,
-        database: process.env.DB_NAME,
-        user:     process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-      }
-);
+const dbUrl = process.env.DATABASE_URL;
+
+const pool = dbUrl
+  ? new Pool({
+      connectionString: dbUrl,
+      ssl: dbUrl.includes("localhost") ? false : { rejectUnauthorized: false },
+    })
+  : new Pool({
+      host:     process.env.DB_HOST,
+      port:     Number(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
 
 export default pool;
